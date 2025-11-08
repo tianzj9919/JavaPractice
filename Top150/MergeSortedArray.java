5d6b89b57b46f78a754772147a116440d18903da
Zhenjiang Tian <ztian@Zhenjiangs-MacBook-Pro.local>
2025-11-08 00:52:34 -0600

    Top150LeetCode

---
+++ Top150/MergeSortedArray.java
@@ -0,0 +1,21 @@
+class Solution{
+    public void merge(int[]nums1, int m, int[]nums2,intn){
+        int len = m + n;
+        while(m!=0&&n!=0){
+            if(nums1[m-1]<nums2[n-1]){
+                nums1[len-1]=nums2[n-1];
+                n--;
+                len--;
+            }else{
+                nums1[len-1]=nums1[m-1];
+                m--;
+                len--;
+            }
+        }
+        while(n!=0){
+            nums1[len-1]=nums2[n-1];
+            n--;
+            len--;
+        }
+    }
+}
